0b0ec61bed964d9edbc052933cd076ce0827cee7
Wanderson Alves <contato@wandersonalves.dev>
2025-08-25 16:37:25 -0300

    feat: setup nativewind

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,7 @@ import { Text, View } from 'react-native'
 
 export default function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View className="flex-1 justify-center items-center">
       <Text>Home Screen</Text>
     </View>
   )
